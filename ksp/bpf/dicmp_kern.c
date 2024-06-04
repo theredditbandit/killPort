f8a4a11830056b4c6b40e3c259d98336f243fc9e
theredditbandit <email@email.com>
2024-06-04 16:12:19 +0000

    fix bug in perf logging

--- ksp/bpf/dicmp_kern.c
+++ ksp/bpf/dicmp_kern.c
@@ -18,7 +18,7 @@ struct perf_trace_event
 #define TYPE_DROP 2
 #define TYPE_PASS 3
 
-#define BLOCKED_PORT 443
+#define BLOCKED_PORT 80
 
 struct
 {
@@ -50,57 +50,62 @@ int xdp_dicmp(struct xdp_md *ctx)
         bpf_perf_event_output(ctx, &output_map, BPF_F_CURRENT_CPU, &e, sizeof(e));
         return XDP_DROP;
     }
-    // Pointers to packet data
-    void *data_end = (void *)(long)ctx->data_end;
-    void *data = (void *)(long)ctx->data;
-
     // Parse Ethernet header
-    struct ethhdr *eth = data;
     if ((void *)(eth + 1) > data_end)
+    {
         e.type = TYPE_PASS;
         __u64 ts = bpf_ktime_get_ns();
         e.processing_time_ns = ts - e.timestamp;
         e.timestamp = ts;
         bpf_perf_event_output(ctx, &output_map, BPF_F_CURRENT_CPU, &e, sizeof(e));
         return XDP_PASS;
+    }
 
     // Only handle IPv4 packets
     if (eth->h_proto != __constant_htons(ETH_P_IP))
+    {
         e.type = TYPE_PASS;
         __u64 ts = bpf_ktime_get_ns();
         e.processing_time_ns = ts - e.timestamp;
         e.timestamp = ts;
         bpf_perf_event_output(ctx, &output_map, BPF_F_CURRENT_CPU, &e, sizeof(e));
         return XDP_PASS;
+    }
 
     // Parse IP header
     struct iphdr *ip = data + sizeof(*eth);
     if ((void *)(ip + 1) > data_end)
+    {
         e.type = TYPE_PASS;
         __u64 ts = bpf_ktime_get_ns();
         e.processing_time_ns = ts - e.timestamp;
         e.timestamp = ts;
         bpf_perf_event_output(ctx, &output_map, BPF_F_CURRENT_CPU, &e, sizeof(e));
         return XDP_PASS;
+    }
 
     // Only handle TCP packets
     if (ip->protocol != IPPROTO_TCP)
+    {
         e.type = TYPE_PASS;
         __u64 ts = bpf_ktime_get_ns();
         e.processing_time_ns = ts - e.timestamp;
         e.timestamp = ts;
         bpf_perf_event_output(ctx, &output_map, BPF_F_CURRENT_CPU, &e, sizeof(e));
         return XDP_PASS;
+    }
 
     // Parse TCP header
     struct tcphdr *tcp = (void *)ip + ip->ihl * 4;
     if ((void *)(tcp + 1) > data_end)
+    {
         e.type = TYPE_PASS;
         __u64 ts = bpf_ktime_get_ns();
         e.processing_time_ns = ts - e.timestamp;
         e.timestamp = ts;
         bpf_perf_event_output(ctx, &output_map, BPF_F_CURRENT_CPU, &e, sizeof(e));
         return XDP_PASS;
+    }
 
     // Check if the destination port is 443
     if (tcp->dest == __constant_htons(BLOCKED_PORT))
@@ -114,7 +119,7 @@ int xdp_dicmp(struct xdp_md *ctx)
         return XDP_DROP;
     }
     // Allow packet to pass
-    e.type = TYPE_DROP;
+    e.type = TYPE_PASS;
     __u64 ts = bpf_ktime_get_ns();
     e.processing_time_ns = ts - e.timestamp;
     e.timestamp = ts;
